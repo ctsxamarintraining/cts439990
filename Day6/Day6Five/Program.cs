a90ff88435bfbddc8c40efc9911b2cf3c861c755
CTS439990 <divyavuppala10@gmail.com>
2015-10-20 10:42:39 +0530

    Day6Assignment

---
+++ Day6/Day6Five/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day6Five
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Dictionary<Product,int> personList1 = new Dictionary<Product,int>(new MyEqualityComparer()){
+				{new Product{id=1,name="Mac"},1},
+				{new Product{id=2,name="iPad"},2},
+				{new Product{id=1,name="Mac"},3}
+			};
+			Console.WriteLine ("IEqualityComparer");
+
+			foreach (Product p in personList1.Keys)
+				Console.WriteLine (p.name);
+			List<Product> myList = new List<Product> {
+				new Product{ id = 1, name = "Mac" },
+				new Product{ id = 6, name = "iPhone" }
+			};
+			Console.WriteLine (" ");
+			Console.WriteLine (" ");
+			Console.WriteLine ("IComparer");
+			myList.Sort (new MyComparer ());
+			foreach(Product p in myList)
+				Console.WriteLine (p.name);
+		}
+	}
+	class Product{
+		public string name;
+		public int id;
+	}
+	class MyEqualityComparer : IEqualityComparer<Product>{
+		
+		public bool Equals (Product x, Product y)
+		{
+			return x.id==y.id;
+		}
+		public int GetHashCode (Product obj)
+		{
+			return obj.GetHashCode ();
+		}
+
+	}
+	class MyComparer : IComparer<Product>{
+		
+		public int Compare (Product x, Product y)
+		{
+			return x.name.CompareTo (y.name);
+		}
+
+	}
+}
+
---
+++ Day6/Day6Four/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day6Four
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			List<string> AList = new List<string>();
+			AList.Add ("A");
+			Console.WriteLine ("Elements in list are");
+			foreach (string ele in AList)
+				Console.WriteLine (ele);
+			AList.AddRange (new List<string> (){ "B", "C", "D" });
+			Console.WriteLine ("Elements in list are");
+			foreach (string ele in AList)
+				Console.WriteLine (ele);
+			AList.Remove ("B");
+			Console.WriteLine ("Element deleted is B");
+			Console.WriteLine ("Elements in list are");
+			foreach (string ele in AList)
+				Console.WriteLine (ele);
+			AList.Insert (1,"F");
+			Console.WriteLine ("Elements in list are");
+			foreach (string ele in AList)
+				Console.WriteLine (ele);
+			AList.Clear ();
+			Console.WriteLine ("list is cleared");
+
+
+
+			Dictionary<int,string> ADictionary = new Dictionary<int, string> (){{1,"Divya"},{2,"sravan"}};
+			ADictionary.Add (3, "madhu");
+			Console.WriteLine ("Keys in Dictionary are");
+			foreach (int ele in ADictionary.Keys)
+				Console.WriteLine (ele);
+			Console.WriteLine ("Values in Dictionary are");
+			foreach (string ele in ADictionary.Values)
+				Console.WriteLine (ele);
+			ADictionary.ContainsValue ("madhu");
+			ADictionary.Remove (3);
+			Console.WriteLine ("Keys in Dictionary are");
+			foreach (int ele in ADictionary.Keys)
+				Console.WriteLine (ele);
+			Console.WriteLine ("Values in Dictionary are");
+			foreach (string ele in ADictionary.Values)
+				Console.WriteLine (ele);
+			ADictionary.Clear ();
+		}
+	}
+}
